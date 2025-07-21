d2166287ad9b5aefb831375e3c6d68de0db48591
oodafeisrael <oodafeisrael@yahoo.com>
2025-07-21 11:31:14 +0000

    test file updated

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -62,9 +62,9 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', 
-                          return_value=42)
-        as mock_method:
+        with patch.object(
+            TestClass, "a_method", return_value=42
+        ) as mock_method:
             test_obj = TestClass()
 
             # Call the memoized property twice
