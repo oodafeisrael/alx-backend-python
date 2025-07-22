3b994b8553bdbb85a8003a5d77da51eed378b622
oodafeisrael <oodafeisrael@yahoo.com>
2025-07-22 18:05:45 +0000

    file update

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -75,6 +75,15 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(
                 result, 'https://api.github.com/orgs/testorg/repos')
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test that has_license returns the correct result"""
+        self.assertEqual(
+                GithubOrgClient.has_license(repo, license_key), expected)
+
 
 if __name__ == "__main__":
     unittest.main()
