5cfa2fc706df4039a34ff18c684918d7ea1db677
oodafeisrael <oodafeisrael@yahoo.com>
2025-07-27 17:56:19 +0100

    tested app

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from chats.views import ConversationViewSet, MessageViewSet
+from chats.urls import router as chats_router, convo_router
 
 # Swagger/OpenAPI
 from drf_yasg.views import get_schema_view
