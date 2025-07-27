24731550abfd85881400ae1c11870907c65002fb
oodafeisrael <oodafeisrael@yahoo.com>
2025-07-27 17:14:53 +0100

    urls.py file updated

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -6,6 +6,7 @@ This sets up API routes using Django REST Framework's DefaultRouter.
 from django.urls import path, include
 from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
 
 # Create a defaultRouter instance using routers.DefaultRouter()
 router = routers.DefaultRouter()
@@ -17,7 +18,14 @@ router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
 
+# Nested router for messages under conversations
+convo_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
 # URL patterns using the router
+
+all_routes = router.urls + convo_router.urls
+
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(all_routes)),
 ]
