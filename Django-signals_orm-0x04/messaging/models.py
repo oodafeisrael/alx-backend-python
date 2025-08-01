69a177accce9cd85e65ef8dd7f38e58a9d41ff0f
oodafeisrael <oodafeisrael@yahoo.com>
2025-08-01 14:10:12 +0100

    models file updated

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -51,3 +51,21 @@ class MessageHistory(models.Model):
 
     def __str__(self):
         return f"Edit History for Message {self.message.id} at {self.edited_at}"
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(
+        User,
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='edited_messages'
+    )
+
+    def __str__(self):
+        return f"From {self.sender} to {self.receiver} - {self.content[:30]}"
